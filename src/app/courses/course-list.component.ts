70ffb3b0a49f550f477e0e37f24eed3ea5ba54de
Alan Felipe Aliske <alanfelipealiske@gmail.com>
2022-05-03 21:04:07 -0300

    update

---
+++ src/app/courses/course-list.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { Course } from './course'
+import { CourseServise } from './course.servise';
+
+@Component({
+    selector: 'app-course-list',
+    templateUrl: './course-list.component.html'
+})
+export class CourseListComponent implements OnInit {
+
+    filteredCourses: Course[] = [];
+    _courses: Course[] = [];
+    _filterBy: string = '';
+
+    constructor(private courseServise: CourseServise) { }
+
+    ngOnInit(): void {
+        this._courses = this.courseServise.retrieveAll();
+        this.filteredCourses = this._courses;
+    }
+
+    set filter(value: string){
+        this._filterBy = value;
+        this.filteredCourses = this._courses.filter((course: Course) => course.name.toLowerCase().indexOf(this._filterBy.toLowerCase()) > -1);
+    }
+
+    get filter(){
+        return this._filterBy;
+    }
+
+}
